6b2325c6293b8bc75facf5dc1658bb582094d0d6
Mike Russell <mike@musicradiocreative.com>
2017-01-04 16:32:24 +0000

    first commit

---
+++ tideupdate.py
@@ -0,0 +1,93 @@
+import time
+from tidestore import *
+
+if hl == "3":
+	# 3 tide times today
+	timenow = (time.strftime("%H:%M"))
+	FMT = '%H:%M'
+	if hl0 == "High":
+		from datetime import datetime, time
+		if datetime.strptime(tt0, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt0
+			nexthighm = th0
+		elif datetime.strptime(tt2, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt2
+			nexthighm = th2
+		else:
+			nexthightide = tt4
+			nexthighm = th4
+		if datetime.strptime(tt1, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt1
+			nextlowm = th1
+		else:
+			nextlowtide = tt3
+			nextlowm = tt3
+	else:
+		from datetime import datetime, time
+		if datetime.strptime(tt0, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt0
+			nextlowm = th0
+		elif datetime.strptime(tt2, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt2
+			nextlowm = th2
+		else:
+			nextlowtide = tt4
+			nextlowm = th4
+		if datetime.strptime(tt1, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt1
+			nexthighm = th1
+		else:
+			nexthightide = tt3
+			nexthighm = th3
+elif hl == "4":
+	# 4 tide times today
+	timenow = (time.strftime("%H:%M"))
+	FMT = '%H:%M'
+	if hl0 == "High":
+		from datetime import datetime, time
+		if datetime.strptime(tt0, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt0
+			nexthighm = th0
+		elif datetime.strptime(tt2, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt2
+			nexthighm = th2
+		else:
+			nexthightide = tt4
+			nexthighm = th4
+		if datetime.strptime(tt1, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt1
+			nextlowm = th1
+		elif datetime.strptime(tt3, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt3
+			nextlowm = th3
+		else:
+			nextlowtide = tt5
+			nextlowm = tt5
+	else:
+		from datetime import datetime, time
+		if datetime.strptime(tt0, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt0
+			nextlowm = th0
+		elif datetime.strptime(tt2, FMT) > datetime.strptime(timenow, FMT):
+			nextlowtide = tt2
+			nextlowm = th2
+		else:
+			nextlowtide = tt4
+			nextlowm = th4
+		if datetime.strptime(tt1, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt1
+			nexthighm = th1
+		elif datetime.strptime(tt3, FMT) > datetime.strptime(timenow, FMT):
+			nexthightide = tt3
+			nexthighm = th3
+		else:
+			nexthightide = tt5
+			nexthighm = th5
+else:
+	# Tide data not present or incorrect
+	print "ERROR: No tide data present."
+
+print "Next High Tide: %s" % nexthightide
+print "Next High Tide Height: %s" % nexthighm
+print "Next Low Tide: %s" % nextlowtide
+print "Next Low Tide Height: %s" % nextlowm
